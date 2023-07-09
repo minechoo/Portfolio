e7cbb8dcaa740bab7b1aed4602ad179db83dc5db
minechoo <choominhae@gmail.com>
2023-07-09 10:53:10 +0900

    모바일 메뉴 width

--- js/header.js
+++ js/header.js
@@ -5,9 +5,11 @@ const btnG = document.querySelector('#close_button');
 menu.addEventListener('click', () => {
 	gnb.classList.remove('off');
 	gnb.classList.add('on');
+	document.body.style.overflow = 'hidden';
 });
 
 btnG.addEventListener('click', () => {
 	gnb.classList.remove('on');
 	gnb.classList.add('off');
+	document.body.style.overflow = 'auto';
 });
